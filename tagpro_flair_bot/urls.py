3ac152a9eb1f4da3a6126e7bdd9eb8ac52d2a6ae
Precious Roy <TagPro-PreciousRoy@users.noreply.github.com>
2014-06-10 19:40:08 -0400

    Allow setting flair.

--- tagpro_flair_bot/urls.py
+++ tagpro_flair_bot/urls.py
@@ -1,14 +1,14 @@
-from django.views.generic import TemplateView
 from django.conf.urls import patterns, include, url
 
-from views import auth_tagpro, deauth_tagpro
+from views import auth_tagpro, deauth_tagpro, set_flair, HomeView
 
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    url(r'^$', TemplateView.as_view(template_name="home.html"), name="home"),
+    url(r'^$', HomeView.as_view(), name="home"),
     url(r'^tp_auth/$', auth_tagpro, name="auth_tagpro"),
     url(r'^tp_logout/$', deauth_tagpro, name="deauth_tagpro"),
+    url(r'^set_flair/(?P<flair>[-\w]+)/$', set_flair, name="set_flair"),
     url('', include('social.apps.django_app.urls', namespace='social')),
 )
--- tagpro_flair_bot/views.py
+++ tagpro_flair_bot/views.py
@@ -1,21 +1,50 @@
-import requests
 from BeautifulSoup import BeautifulSoup
-from django.shortcuts import redirect
+import praw
+import requests
+from django.conf import settings
+from django.contrib import messages
 from django.core.urlresolvers import reverse
+from django.shortcuts import redirect
+from django.views.generic import TemplateView
 from social_auth.pipeline import deauth_tagpro as deauth_tagpro_pipeline
 
-def get_flairs(html):
+
+reddit_api = praw.Reddit(user_agent=settings.BOT_USER_AGENT)
+reddit_api.login(username=settings.REDDIT_MOD_USERNAME, password=settings.REDDIT_MOD_PASSWORD)
+
+
+FLAIRS_BY_KEY = {v[1]: v[0] for k, v in settings.FLAIRS_BY_POSITION.iteritems()}
+
+
+class HomeView(TemplateView):
+    template_name = "home.html"
+    
+    def get_context_data(self):
+        context = super(HomeView, self).get_context_data()
+        context.update({'FLAIRS_BY_KEY': FLAIRS_BY_KEY})
+        return context
+
+
+def parse_available_flair(html):
+    """
+    Helper function which pulls the active flairs from the parsed HTML of a
+    user's profile page.
+    """
     flair_table = html.findAll('table')[2]
     rows = flair_table.findAll('tr')
     flairs = []
-    for tr in rows:
-        if tr.get('class', '') == "fade" or tr.find('th'):
-            # No faded rows or the heading
-            continue
-        flairs.append(tr.findAll('td')[1].getString())
+    for row in rows:
+        icon = row.find('div')
+        if icon and row.get("class", "") != "fade":
+            position = icon['style'][len('background-position: '):]
+            flairs.append(settings.FLAIRS_BY_POSITION[position][1])
     return flairs
 
+
 def auth_tagpro(request):
+    """
+    Verify that the user owns the specified TagPro profile.
+    """
     profile_url = request.POST.get('profile_url')
     token = request.session.get('tagpro_token')
     response = requests.get(profile_url)
@@ -24,9 +53,30 @@ def auth_tagpro(request):
     if tagpro_name == token:
         request.session['tp_authenticated'] = True
         request.session['tp_profile'] = profile_url
-        request.session['available_flair'] = get_flairs(parsed)
+        request.session['available_flair'] = parse_available_flair(parsed)
+    else:
+        messages.error(request, "Your name doesn't match the token!")
     return redirect(reverse('home'))
 
+
 def deauth_tagpro(request):
+    """
+    Unlink the session from the specified TagPro profile.
+    """
     deauth_tagpro_pipeline(request=request)
     return redirect(reverse('home'))
+
+
+def set_flair(request, flair):
+    """
+    Set the user's flair for the subreddit.
+    """
+    if 'available_flair' in request.session and flair in request.session['available_flair'] and flair in FLAIRS_BY_KEY.keys():
+        reddit_api.set_flair(
+            settings.REDDIT_MOD_SUBREDDIT,
+            request.user.username,
+            flair_css_class=flair)
+        messages.success(request, "Flair set!")
+    else:
+        messages.error(request, "Sorry, you can't have that flair :(")
+    return redirect(reverse("home"))
