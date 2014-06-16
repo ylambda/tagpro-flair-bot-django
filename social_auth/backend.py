b703c0fa27f4eb404907c3cd4e9b76dea9b766f7
Precious Roy <TagPro-PreciousRoy@users.noreply.github.com>
2014-06-15 21:01:44 -0400

    Retry if Reddit errors on the get_user_data step.

--- social_auth/backend.py
+++ social_auth/backend.py
@@ -10,3 +10,15 @@ class RedditOAuth2(DefaultRedditOAuth2):
             'User-Agent': settings.BOT_USER_AGENT
         })
         return headers
+
+    def user_data(self, access_token, *args, **kwargs):
+        retry_count = 0
+        exc = None
+        while retry_count < settings.REDDIT_MAX_RETRIES:
+            try:
+                return super(RedditOAuth2, self).user_data(access_token, *args, **kwargs)
+            except Exception, e:
+                exc = e
+                pass
+        raise exc
+
