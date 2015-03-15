2f9a0a36416e63c1c4f1c5363dc66234441d41eb
Precious Roy <TagPro-PreciousRoy@users.noreply.github.com>
2015-03-15 17:20:08 -0400

    Apparently this is not how to do this.

--- tagpro_flair_bot/settings/development.py
+++ tagpro_flair_bot/settings/development.py
@@ -1,13 +1,5 @@
 from base import *
 
-# Static asset configuration
-STATIC_ROOT = 'static'
-
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
-
-
 try:
     from localsettings import *
 except ImportError:
